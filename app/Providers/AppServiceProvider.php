15fd0e4b8e356bfc3ea7b75c5af07b9662bd0260
sakinamt <sakinamt24@gmail.com>
2024-10-12 18:47:51 +0500

    MINOR: Add resources and collections

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use App\Observers\UserObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,8 +19,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
+
     public function boot(): void
     {
-        //
+        User::observe(UserObserver::class);
     }
+
 }
